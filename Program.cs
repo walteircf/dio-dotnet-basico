d3894a23053be4a3b85c7aeba55a6e37864ed612
Walteir Coelho <wcfreitasf@gmail.com>
2025-07-10 16:40:40 -0300

    adiciona o repositório do projeto de dotnet basico ao repositório

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExemploFundamentos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pessoa vendedor = new Pessoa();
+            Pessoa comprador = new Pessoa();
+
+            Console.Write("Digite o nome do vendedor: ");
+            vendedor.Nome = Console.ReadLine();
+            Console.Write("Digite a idade do vendedor: ");
+            vendedor.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O Vendedor {vendedor.Nome} foi cadastrado com sucesso!");
+
+        }
+    }
+}
