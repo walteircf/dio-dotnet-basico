554b8b5e14865fe89efdfd9c48264b419286e650
Walteir Coelho <wcfreitasf@gmail.com>
2025-07-10 21:01:06 -0300

    adiciona mais informações sobre fundamento do .NET

--- Program.cs
+++ Program.cs
@@ -9,15 +9,54 @@ namespace ExemploFundamentos
     {
         public static void Main(string[] args)
         {
-            Pessoa vendedor = new Pessoa();
-            Pessoa comprador = new Pessoa();
+            //casting
+            //int a = Convert.ToInt32("5"); //Opção 1 de casting usando a classe "Convert"
+            int a = int.Parse("5"); //converte o string para int32
 
-            Console.Write("Digite o nome do vendedor: ");
+            Console.WriteLine(a);
+
+
+
+
+
+
+
+
+            // Pessoa vendedor = new Pessoa();
+            // Pessoa comprador = new Pessoa();
+
+            /*Console.Write("Digite o nome do vendedor: ");
             vendedor.Nome = Console.ReadLine();
             Console.Write("Digite a idade do vendedor: ");
             vendedor.Idade = int.Parse(Console.ReadLine());
 
-            vendedor.Apresentar();
+            vendedor.Apresentar();*/
+
+            // string apresentacao = "Olá, seja bem vindo!";
+            // int quantidade = 1;
+            // double altura = 1.80;
+            // decimal preco = 1.80M;
+            // bool condicao = true;
+
+            // Console.WriteLine(apresentacao);
+            // Console.WriteLine("Valor da variável quantidade: " + quantidade);
+            // Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+            // Console.WriteLine("Valor da variável preço: " + preco);
+            // Console.WriteLine("Valor da variável condição: " + condicao);
+
+            // DateTime dataAtual = DateTime.Now;
+            // Console.WriteLine(dataAtual);
+
+            // int a = 10;
+            // int b = 20;
+
+            // int c = a + b; // sinal de "=" é atribuição
+
+            // Console.WriteLine(c);
+
+            // c += 5; // == c = c  + 5;
+
+            // Console.WriteLine(c);
 
         }
     }
